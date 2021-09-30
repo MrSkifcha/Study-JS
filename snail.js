34835d6bbddcfd90fda9615573fa8063d239bfd8
MrSkifcha <grishakas@yandex.ru>
2021-09-30 17:53:51 +0300

    Snail: finish

--- snail.js
+++ snail.js
@@ -1,14 +1,12 @@
-function main() {
-    var depth = parseInt(readLine(), 10);
-    var day = 0;
-    var total = 0;
-    while (total < depth) {
-        day += 1;
-        total += 7;
-        if (total >= depth) {
-            console.log(day);
-            break;
-        }
-        total = total - 2;
+var depth = 32634756;
+var day = 0;
+var total = 0;
+while (total < depth) {
+    day += 1;
+    total += 7;
+    if (total >= depth) {
+        console.log(day);
+        break;
     }
+    total = total - 2;
 }
