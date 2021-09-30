364c6a92eead2627d3f1fe7ba6156d72938c1a82
MrSkifcha <grishakas@yandex.ru>
2021-09-30 16:54:32 +0300

    FizzBuzz: finished task

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,13 +2,13 @@ var i = 1;
 var multiples3 = 'Fizz';
 var multiples5 = 'Buzz';
 for (i; i < 101; i++) {
-    if (i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 !== 0) {
         console.log(i + multiples3);
-    } else if (i % 5 == 0) {
+    } if (i % 5 == 0 && i % 3 !== 0) {
         console.log(i + multiples5);
-    }/* if (i % 3 == 0 && i % 5 == 0){
-        console.log(multiples3 + multiples5);
-    }*/ else {
+    } if (i % 3 == 0 && i % 5 == 0){
+        console.log(i + multiples3 + multiples5);
+    } else {
         console.log(i);
     }
 }
